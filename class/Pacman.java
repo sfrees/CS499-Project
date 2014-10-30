37b09170342373a5c62ef1d8ada09791a728f730
stephenfrees <stephenf93@gmail.com>
2014-10-30 13:37:35 -0500

    Added a pacman class

---
+++ class/Pacman.java
@@ -0,0 +1,17 @@
+class Pacman {
+  private double x;
+  private double y;
+  
+  public Pacman() {
+    x = 0.0;
+    y = 0.0;
+  }
+  
+  public static double getX() {
+    return x;
+  }
+  
+  public static double getY() {
+    return y;
+  }
+}
