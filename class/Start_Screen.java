b11af3d46070b3b26da71fdd025aa985e5af1d03
Eric <eching@uab.edu>
2014-12-03 23:51:53 -0600

    Added Text
    Just added some text for the start screen

--- class/Start_Screen.java
+++ class/Start_Screen.java
@@ -10,18 +10,31 @@ public class Start_Screen extends JFrame implements ActionListener{
 	
 	public Start_Screen(){
 		//Creates the frame
-	    Start = new JFrame("PacMan Start");
+	    Start = new JFrame("Simple Start");
 	    Start.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
 	    Start.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    Start.setSize(400,400);
 	    Start.setResizable(false);
 	    Start.setVisible(true);	
 			
+	    //Create the title
+	    JLabel label = new JLabel("PacMan");
+	    label.setForeground(Color.yellow);
+	    label.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+	    label.setFont(new Font(null, Font.BOLD, 75));
+	    label.setBounds(60, 50, 290, 80);
+	   
+	    //Simple display of lives
+	    JLabel lives = new JLabel("Lives: 3");
+	    lives.setForeground(Color.black);
+	    lives.setFont(new Font(null, Font.BOLD, 20));
+	    lives.setBounds(170, 275, 100, 100);
+	    
 		//creates the start button
 	    begin = new JButton("Start");
 	    begin.setBackground(Color.yellow);
-	    begin.setBounds(130,130,150,50);
-	    begin.setFont(new Font(null, Font.BOLD, 50));    
+	    begin.setBounds(130,200,150,50);
+	    begin.setFont(new Font(null, Font.BOLD, 40));    
 	    begin.setBorder(BorderFactory.createLineBorder(Color.black, 2));
 	    begin.addActionListener(this);
 	    add(begin);
@@ -29,7 +42,7 @@ public class Start_Screen extends JFrame implements ActionListener{
 	    //creates the quit button
 	    end = new JButton("Quit");
 	    end.setBackground(Color.yellow);
-	    end.setBounds(180,180,50,25);
+	    end.setBounds(180,250,50,25);
 	    end.setFont(new Font(null, Font.BOLD, 20));
 	    end.setBorder(BorderFactory.createLineBorder(Color.black, 2));
 	    end.addActionListener(this);
@@ -42,14 +55,17 @@ public class Start_Screen extends JFrame implements ActionListener{
 	    StartButtons.setSize(400, 400);
 	    StartButtons.setBackground(Color.blue);
 	    StartButtons.add(begin);  
-	    StartButtons.add(end);   
-	    start.add(StartButtons, 1);	
+	    StartButtons.add(end);
+	    StartButtons.add(label);
+	    StartButtons.add(lives);
+	    start.add(StartButtons);	
 	}
 	public void actionPerformed(ActionEvent e) {	
 		if(e.getSource() == begin){
 			//Can't figure out how to make the game to start by calling the room
 			//using what is below the program will lock up.
-			//Room r = new Room();
+			//Room r = new Room();//Will lock up on my system
+			
 			System.exit(0);
 		}else if(e.getSource() == end){
 			Start.dispose();
